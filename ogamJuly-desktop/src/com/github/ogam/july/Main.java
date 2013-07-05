7f1b6f4728430251467f26a8edf98b57aa59ad28
Claus Aranha <caranha@gmail.com>
2013-07-05 18:44:26 +0900

    Added gdx-ui setup created files

---
+++ ogamJuly-desktop/src/com/github/ogam/july/Main.java
@@ -0,0 +1,16 @@
+package com.github.ogam.july;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "ogamJuly";
+		cfg.useGL20 = false;
+		cfg.width = 480;
+		cfg.height = 320;
+		
+		new LwjglApplication(new OgamJulyMain(), cfg);
+	}
+}
